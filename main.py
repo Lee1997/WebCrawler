14d99a66a562d6b2029c6443cfc5eaf869e135e1
Lee1997 <leelaffan97@gmail.com>
2018-09-25 14:09:52 +0100

    Make generic

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from spider import Spider
 from domain import *
 from General import *
 
-PROJECT_NAME = 'Arteezy'
-HOMEPAGE = 'http://dotamax.com/player/match/86745912/?skill=pro'
+PROJECT_NAME = 'Your_project_name'
+HOMEPAGE = 'http://google.com/'
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
 QUEUE_FILE = PROJECT_NAME + "/queue.txt"
 CRAWLED_FILE = PROJECT_NAME + "/crawled.txt"
